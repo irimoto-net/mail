20fa00575c9761bcebb4fd2d92f5a74b2ee455d3
EC2 Default User <ec2-user@ip-172-31-30-11.ap-southeast-1.compute.internal>
2019-02-01 09:20:40 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   
-    config.action_mailer.default_url_options = { host: localhost, port: 3000 }
+    config.action_mailer.default_url_options = { host: ENV['HOST_NAME'] }
     config.action_mailer.raise_delivery_errors = false
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
